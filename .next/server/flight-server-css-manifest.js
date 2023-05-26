9cadb175c0e52386d5e5415d426aa78cfa0167ce
marovichn <nikolamarovic.11@gmail.com>
2023-05-26 22:36:03 +0200

    small-fix

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\Home\\Desktop\\Web development\\Searchingo\\share-prompts\\app\\layout.js": [
+      "C:\\Users\\Home\\Desktop\\Web development\\Searchingo\\share-prompts\\styles\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\Home\\Desktop\\Web development\\Searchingo\\share-prompts\\app\\page": [
+      "C:\\Users\\Home\\Desktop\\Web development\\Searchingo\\share-prompts\\styles\\globals.css"
+    ]
+  }
+}
